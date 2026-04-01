5ae00aa5e05513f288c6683e4420ddc508bfbe55
Damian Vega Meza <damian@LAPTOP-H44S0OTB.localdomain>
2026-04-01 15:30:29 -0700

    Assignment done

--- main_read.cpp
+++ main_read.cpp
@@ -11,16 +11,23 @@ int main()
     ifstream ifs;
 
     // TODO: open "students.txt" for reading
-
-    // TODO: check if file opened successfully; print error and exit if not
-
+    ifs.open("Students.txt");
+    if(!ifs.is_open()) {
+        cerr <<"Error opening file" << endl;
+        return 1;
+    }
     // TODO: read M from ifs, print "Total M students"
-
+    ifs >> M;
+    cout <<"Total " << M << " students "<< endl;
     for (int i = 0; i < M; i++)
     {
         // TODO: read stuName, score1, score2 from ifs
         // TODO: compute sum = score1 + score2, avg = sum / 2.0
         // TODO: print Student Name, score1, score2, Sum, Avg
+        ifs >> stuName >> score1 >> score2;
+        sum = score1 + score2;
+        avg = sum / 2.0;
+        cout << stuName <<" Sum: " << sum << " Avg: " << avg << endl;
     }
     ifs.close();
     return 0;
